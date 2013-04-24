26860802d98dc3ee350c0d2daa47a82087794259
James Lamont <james@semblancesystems.com>
2013-04-24 20:59:50 +1000

    Update event.php

--- event.php
+++ event.php
@@ -1,24 +1,28 @@
 <?php
   // ---- Edit these attributes
   $auth_token = 'AUTH_TOKEN'; // Get from https://www.getvero.com/account
-  $customer_id = '12345';
-  $event_name = 'Purchased product'
+  $user_id    = '12345';
+  $event_name = 'Purchased product';
 
-
-
-  // ---- Example of making a REST call to Vero
-  $required_keys = array(
-    'auth_token' => $auth_token,
-    'identity' => {
-      'id' => $customer_id
-    },
-    'event_name' => $event_name,
-    'development_mode' => 'false'
+  // ---- Optional: Specify any event properties
+  $event_data = array(
+    "sku" => "12221",
+    "unit_cost" => 42.00
   );
 
-  $json_data    = json_encode($required_keys);
-  $endpoint = 'https://www.getvero.com/api/v2/events/track';
+  // ---- Example of making a REST call to Vero
+  $request_data = array(
+    'auth_token'        => $auth_token,
+    'identity'          => array('id' => $user_id),
+    'event_name'        => $event_name,
+    'data'              => $event_data,
+    'development_mode'  => 'false'
+  );
+  $request_data    = json_encode($request_data);
+
+  $endpoint = 'https://www.getvero.com/api/v2/events/track.json';
   $headers = array('Accept: application/json', 'Content-Type: application/json');
+
   $handle  = curl_init();
   curl_setopt($handle, CURLOPT_URL, $endpoint);
   curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
@@ -26,7 +30,7 @@
   curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
   curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($handle, CURLOPT_POST, true);
-  curl_setopt($handle, CURLOPT_POSTFIELDS, $json_data);
+  curl_setopt($handle, CURLOPT_POSTFIELDS, $request_data);
 
   $result = curl_exec($handle);
   $code   = curl_getinfo($handle, CURLINFO_HTTP_CODE);

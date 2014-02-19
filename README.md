7ab59495d2acb658b5e762bac603a72e165d9be8
Damien Brzoska <damien.brz@gmail.com>
2014-02-19 12:29:12 +1100

    Fix Library

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ Vero for PHP
 Vero PHP client is using cURL on top on our Rest API. Here's how to use it:
 
 ```php
-$v = new Vero\Client("YOUR_AUTH_TOKEN");
+$v = new Vero("YOUR_AUTH_TOKEN");
+
 $v->identify("1234567890", "jeff@yourdomain.com", array('First name' => 'Jeff', 'Last name' => 'Kane'));
-$v->track("View Product", array('identifier' => '1234567890'), array('Product name' => 'Blue shoes', 'Price' => 99));
+$v->update("1234567890", array('First name' => 'Jack', 'Last name' => 'Wollo', 'job_title' => 'Developer'));
+$v->tags('1234567890', array('Blog reader'), array());
+
+$v->track("Subscribe to service", array('id' => '1234567890'), array('Plan name' => 'XL Plan', 'Price' => 149));
+
 $v->unsubscribe("1234567890");
 ```
--- vero/client.php
+++ vero/client.php
@@ -15,10 +15,10 @@
     public function identify($user_id, $email, $data) {
       $endpoint = "https://api.getvero.com/api/v2/users/track.json";
       $request_data = array(
-        'auth_token'        => $auth_token,
+        'auth_token'        => $this->auth_token,
         'id'                => $user_id,
         'data'              => $data,
-        'development_mode'  => $development_mode
+        'development_mode'  => $this->development_mode
       );
 
       if ($email)
@@ -30,32 +30,32 @@
     public function update($user_id, $changes) {
       $endpoint = "https://api.getvero.com/api/v2/users/edit.json";
       $request_data = array(
-        'auth_token'        => $auth_token,
+        'auth_token'        => $this->auth_token,
         'id'                => $user_id,
         'changes'           => $changes,
-        'development_mode'  => $development_mode
+        'development_mode'  => $this->development_mode
       );
 
-      return $this->_send($endpoint, $request_data);
+      return $this->_send($endpoint, $request_data, 'put');
     }
 
     public function tags($user_id, $add, $remove) {
       $endpoint = "https://api.getvero.com/api/v2/users/tags/edit.json";
       $request_data = array(
-        'auth_token'        => $auth_token,
+        'auth_token'        => $this->auth_token,
         'id'                => $user_id,
         'add'               => $add,
         'remove'            => $remove,
-        'development_mode'  => $development_mode
+        'development_mode'  => $this->development_mode
       );
 
-      return $this->_send($endpoint, $request_data);
+      return $this->_send($endpoint, $request_data, 'put');
     }
 
     public function unsubscribe($user_id) {
       $endpoint = "https://api.getvero.com/api/v2/users/unsubscribe.json";
       $request_data = array(
-        'auth_token'        => $auth_token,
+        'auth_token'        => $this->auth_token,
         'id'                => $user_id
       );
 
@@ -65,16 +65,17 @@
     public function track($event_name, $identity, $data) {
       $endpoint = "https://api.getvero.com/api/v2/events/track.json";
       $request_data = array(
-        'auth_token'        => $auth_token,
+        'auth_token'        => $this->auth_token,
         'identity'          => $identity,
+        'event_name'        => $event_name,
         'data'              => $data,
-        'development_mode'  => $development_mode
+        'development_mode'  => $this->development_mode
       );
 
       return $this->_send($endpoint, $request_data);
     }
 
-    private function _send($endpoint, $request_data) {
+    private function _send($endpoint, $request_data, $request_type = 'post') {
       $request_data = json_encode($request_data);
 
       $headers  = array('Accept: application/json', 'Content-Type: application/json');
@@ -85,7 +86,11 @@
       curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
       curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($handle, CURLOPT_POST, true);
+      if ($request_type == 'post') {
+        curl_setopt($handle, CURLOPT_POST, true);
+      } elseif ($request_type == 'put') {
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "PUT");
+      }
       curl_setopt($handle, CURLOPT_POSTFIELDS, $request_data);
 
       $result = curl_exec($handle);
